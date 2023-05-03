d673cb74987b684b7f3f03eb637368bd2acd0d2e
blint33 <lintbraden@gmail.com>
2023-05-03 16:06:49 -0400

    Update senderSu.py

--- SuPi/senderSu.py
+++ SuPi/senderSu.py
@@ -10,8 +10,8 @@ CERTS = '/etc/ssl/certs/ca-certificates.crt'
 BROKER = 'iot.cs.calvin.edu'
 
 # Note: these constants must be set for broker authentication
-USERNAME = 'cs326'   # broker authentication username
-PASSWORD = 'piot'   # broker authentication password
+USERNAME = ''   # broker authentication username
+PASSWORD = ''   # broker authentication password
 
 #prints to terminal whenever MQTT publishes
 def on_publish(client, userdata, mid):
